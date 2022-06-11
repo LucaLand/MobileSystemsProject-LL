d7bb49648c439bb50676cfe489ac8f59c5beee3c
LucaLand <luca.landolfi3@studio.unibo.it>
2022-06-11 10:55:34 +0200

    [V2.8] - Some Modify

--- app/src/main/java/it/unibo/mobilesystems/debugUtils/DebuggerContextNameAnnotation.kt
+++ app/src/main/java/it/unibo/mobilesystems/debugUtils/DebuggerContextNameAnnotation.kt
@@ -1,5 +1,9 @@
 package it.unibo.mobilesystems.debugUtils
 
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Retention(RetentionPolicy.RUNTIME)
 annotation class DebuggerContextNameAnnotation(val contextName : String)
