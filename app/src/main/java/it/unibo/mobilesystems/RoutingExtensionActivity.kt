c134fdb4f3c1616344135b9a64a68ce283fb5c84
LucaLand <luca.landolfi3@studio.unibo.it>
2022-06-22 11:20:52 +0200

    [V0.7.4] - Added Go Button function and Search Button Function (Working)

--- app/src/main/java/it/unibo/mobilesystems/RoutingExtensionActivity.kt
+++ app/src/main/java/it/unibo/mobilesystems/RoutingExtensionActivity.kt
@@ -2,6 +2,7 @@ package it.unibo.mobilesystems
 
 
 
+import android.location.Address
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
@@ -14,6 +15,7 @@ import it.unibo.mobilesystems.geo.Geocoder
 import it.unibo.mobilesystems.geo.GeocoderViewModel
 import it.unibo.mobilesystems.geo.PathCalculator
 import it.unibo.mobilesystems.geo.PathViewModel
+import it.unibo.mobilesystems.utils.hideKeyboard
 import org.osmdroid.bonuspack.location.GeocoderGraphHopper
 import org.osmdroid.bonuspack.location.GeocoderNominatim
 import org.osmdroid.bonuspack.location.NominatimPOIProvider
@@ -33,6 +35,7 @@ private const val ACTIVITY_NAME = "RoadRouting Activity"
 class RoutingExtensionActivity : MainMapsActivity(), MapEventsReceiver {
 
 
+
     protected lateinit var mLocationManager: LocationManager
     protected lateinit var locationService: FusedLocationProviderClient
     protected lateinit var pathCalculator: PathCalculator
@@ -43,7 +46,7 @@ class RoutingExtensionActivity : MainMapsActivity(), MapEventsReceiver {
     private lateinit var pathViewModel: PathViewModel
     private lateinit var geocoder: Geocoder
     private lateinit var geocoderViewModel: GeocoderViewModel
-    private lateinit var nominatim: Geocoder
+
 
     private var destinationPoint: GeoPoint? = null
 
@@ -51,6 +54,7 @@ class RoutingExtensionActivity : MainMapsActivity(), MapEventsReceiver {
     //UI Components
     lateinit var destinationEditText: AutoCompleteTextView
     lateinit var goButton: Button
+    lateinit var searchButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,6 +80,41 @@ class RoutingExtensionActivity : MainMapsActivity(), MapEventsReceiver {
 
         //UI Components
         destinationEditText = findViewById(R.id.destinationEditText)
+        goButton = findViewById(R.id.destinationGoButton)
+        searchButton = findViewById(R.id.destinationSearchButton)
+
+        goButton.setOnClickListener {
+            goOnDest()
+            hideKeyboard(applicationContext, it)
+        }
+        searchButton.setOnClickListener {
+            geocoderViewModel.asyncGetPlacesFromLocation(destinationEditText.text.toString())
+            hideKeyboard(applicationContext, it)
+        }
+
+
+
+        geocoderViewModel.addUpdateUiOnResult { result ->
+            result.onSuccess { addressList ->
+                if(addressList.isNotEmpty()) {
+                    destinationEditText.setAdapter(ArrayAdapter(this, android.R.layout.simple_list_item_1, addressList.map {
+                        var str = ""
+                        for(i in 0..it.maxAddressLineIndex){
+                            str +="${it.getAddressLine(i).trim()}, "
+                        }
+                        return@map str
+                    }))
+                    destinationEditText.showDropDown()
+                    Debugger.printDebug(ACTIVITY_NAME, "Finished Address Search!")
+
+                    destinationEditText.setText(addressToString(addressList[0]))
+                    val destPoint = GeoPoint(addressList[0].latitude, addressList[0].longitude)
+                    focusOnGeoPoint(destPoint)
+                }
+            }
+        }
+        /** AutoComplete text Code (some problems: -slow update; -Not working selecting item)**/
+        /*
         geocoderViewModel.addUpdateUiOnResult { result ->
             result.onSuccess { addressList ->
                 destinationEditText.setAdapter(ArrayAdapter(this, android.R.layout.simple_list_item_1, addressList.map {
@@ -97,11 +136,17 @@ class RoutingExtensionActivity : MainMapsActivity(), MapEventsReceiver {
                 Toast.makeText(this, "Finished Search!", 2).show()
             }
         }
+
+
+
         destinationEditText.doOnTextChanged { text, start, before, count ->
+            if(count > 6){
+                Debugger.printDebug(ACTIVITY_NAME, "Starting AsyncSearch Location!")
                 geocoderViewModel.asyncGetPlacesFromLocation(text.toString())
+            }
         }
 
-        destinationEditText.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        destinationEditText.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
                 view: View?,
@@ -120,25 +165,50 @@ class RoutingExtensionActivity : MainMapsActivity(), MapEventsReceiver {
             }
 
             override fun onNothingSelected(parent: AdapterView<*>?) {
-                //TODO("Not yet implemented")
+
             }
         }
+         */
 
-        goButton = findViewById(R.id.destinationGoButton)
-        goButton.setOnClickListener { goOnDest() }
+    }
+
+    private fun focusOnGeoPoint(selectedPosition: GeoPoint) {
+
+        map.controller.zoomTo(15, 500)
+        map.controller.animateTo(selectedPosition)
+        addMarker(selectedPosition, "Destination")
+        Debugger.printDebug(ACTIVITY_NAME, "Address Selected (${selectedPosition.longitude}, ${selectedPosition.latitude})")
+        this@RoutingExtensionActivity.destinationPoint = selectedPosition
+    }
+
+    private fun addressToString(addr : Address): String {
+        var str = ""
+        for(i in 0 until addr.maxAddressLineIndex){
+            str = str + addr.getAddressLine(i) +", "
+        }
+        return str + addr.getAddressLine(addr.maxAddressLineIndex)
     }
 
     private fun goOnDest() {
-        if(destinationPoint != null)
+        if(destinationPoint != null && mLocationOverlay.myLocation != null) {
             calculatePathToPoint(destinationPoint!!)
+            map.controller.zoomTo(18, 400)
+            mLocationOverlay.enableFollowLocation()
+            mLocationOverlay.enableAutoStop = false
+            val dlat = mLocationOverlay.myLocation.latitude - destinationPoint!!.latitude
+            val dlon = mLocationOverlay.myLocation.longitude - destinationPoint!!.longitude
+            val angle = atan2(dlat, dlon).toFloat()
+            map.setMapOrientation(angle, true)
+            map.invalidate()
+        }
         else
-            Toast.makeText(this, "Insert Destination!!", 4).show()
+            Toast.makeText(this, "Insert Destination!! or Position is Null", 4).show()
     }
 
     override fun onLocationChanged(p0: Location) {
         super.onLocationChanged(p0)
         if(road != null){
-
+            //TODO(NOT yet implemented!)
         }
     }
 
@@ -223,8 +293,7 @@ class RoutingExtensionActivity : MainMapsActivity(), MapEventsReceiver {
     }
 
     override fun singleTapConfirmedHelper(p: GeoPoint?): Boolean {
-        //TODO("Not yet implemented")
-        Debugger.printDebug(ACTIVITY_NAME, "singletapConfirmedHelper(p: $p)")
+        //Debugger.printDebug(ACTIVITY_NAME, "singletapConfirmedHelper(p: $p)")
         return true
     }
 
